4ba71994997556fbf80d5b9a8e3494e3b8f56279
nnkhoa <n.nhukhoa2209@gmail.com>
2019-08-07 15:53:13 +0200

    changed default value from 'search' to 'date_start'

--- arghandler.py
+++ arghandler.py
@@ -2,8 +2,8 @@ import argparse
 
 def parse_argv():
     parser = argparse.ArgumentParser(prog='promed_crawler', description='Process input for searching ProMed\'s database')
-    parser.add_argument('--search', required = False, default = 'anthrax', help = 'search for disease')
-    parser.add_argument('--date-start', required = False, default = '', help = 'start of search range (format YYYY-MM-DD)')
+    parser.add_argument('--search', required = False, default = '', help = 'search for disease')
+    parser.add_argument('--date-start', required = False, default = '2019-01-01', help = 'start of search range (format YYYY-MM-DD)')
     parser.add_argument('--date-end', required = False, default = '', help = 'end of search range (format YYYY-MM-DD)')
     
     args = parser.parse_args()
